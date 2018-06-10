c9b1a22ec3ecea8c7f91ead8952274bf4bfd9e90
cprice <cprice@tutanota.com>
2018-06-10 10:30:00 +0200

    [Rxvt] more configuration

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@
 echo "[X resource] deploying main resource file to your home folder"
 rm /home/$USER/.Xresources
 cp .Xresources /home/$USER
+echo "[Rxvt] installing extensions for terminal"
+sudo cp apps/rxvt.d/newterm /usr/lib/urxvt/perl/
 xrdb ~/.Xresources
